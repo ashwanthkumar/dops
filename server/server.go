0e5b45316940be1ff65972710639835b20120a1e
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-06-26 23:20:57 +0530

    WIP - Empty Server implementation for the build to pass

---
+++ server/server.go
@@ -0,0 +1,13 @@
+package server
+
+// Server is the D-Ops Registry server which acts as a local cache registry and also seeds
+// the images for a specified duration
+type Server struct{}
+
+// Start - Starts the Server
+func (s *Server) Start() {
+}
+
+// Stop - Stops the server (for clean shutdown)
+func (s *Server) Stop() {
+}
